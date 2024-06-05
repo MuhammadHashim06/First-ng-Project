b8154f3d82e3ffd9405398645393d9cbdc3afa3d
MuhammadHashim06 <muhammad.hashimsid@gmail.com>
2024-06-05 15:33:51 +0500

    Create menu with routing link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AddproductComponent } from './addproduct/addproduct.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+    path:'',
+    component:HomeComponent
+},{
+    path:'add-product',
+    component:AddproductComponent
+},{
+    path:'about',
+    component:AboutComponent
+}];
